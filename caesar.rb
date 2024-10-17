74408f35dee1c1bd4aa401650ec769e2c344f8f2
Yago <dolmayan@gmail.com>
2024-10-17 21:43:11 +0200

    Remove the explicit return at the end

--- caesar.rb
+++ caesar.rb
@@ -1,9 +1,9 @@
 def caesar_cipher(text, shift_factor = 5)
-  # Convert the text to an array of ints
+  # Convert the text to an array of ints (ASCII code)
   array_of_chars = text.chars  
   array_of_ints = array_of_chars.map { |char| char.ord }
 
-  # Add to each of the int, the factor (check if it's a letter of the alphabet)
+  # Add to each of the ints, the factor (check if it's a letter of the alphabet)
   # Modulo to wrap from a to z (26 letters in English). If not a letter, stays as it is.
   array_of_ciphered_ints = array_of_ints.map do |int|
     if (int > 64 && int < 91)
@@ -18,7 +18,7 @@ def caesar_cipher(text, shift_factor = 5)
   # Convert each int in the array back to string
   array_of_ciphered_chars = array_of_ciphered_ints.map { |int| int.chr }  
   # Convert the array with chars into a single string
-  return array_of_ciphered_chars.join("")
+  array_of_ciphered_chars.join("")
 end
 
 p caesar_cipher("What a string!", 5)
